57ae004f16e2c42573c0a5a29b1f8d2e7bc959c2
JHON GOMEZ <jhonjairogomez@unicesar.edu.co>
2025-03-19 13:49:56 -0500

    Update generarQR.js

--- Script/generarQR.js
+++ Script/generarQR.js
@@ -1,13 +1,15 @@
 function generarQR(nombre, cedula, direccion, telefono, email) {
-    const qrContent = `
-Nombre: ${nombre}
-Cedula: ${cedula}
-Direccion: ${direccion}
-Telefono: ${telefono}
-Correo Electronico: ${email}
-    `;
+    const baseUrl = "https://jhonj-g.github.io/FormQR/mostrarDatos.html"; // URL del nuevo archivo HTML
+    const queryParams = new URLSearchParams({
+        nombre: nombre,
+        cedula: cedula,
+        direccion: direccion,
+        telefono: telefono,
+        email: email
+    }).toString();
+    const qrContent = `${baseUrl}?${queryParams}`;
     const qr = qrcode(0, 'L');
-    qr.addData(qrContent.trim()); // Eliminar espacios innecesarios
+    qr.addData(qrContent); // Agregar el enlace con los datos
     qr.make();
     return qr.createDataURL();
 }
